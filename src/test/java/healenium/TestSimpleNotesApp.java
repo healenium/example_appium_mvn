213646a6444202aebaa828b8dd138dc6247c19a6
Aleksei_Korobov <Aleksei_Korobov@epam.com>
2021-02-11 18:59:53 +0300

    Implement simple mobile app to test Healenium
    - remove installed application after all tests have run
    
    Issue: EPMHLM-48

--- src/test/java/healenium/TestSimpleNotesApp.java
+++ src/test/java/healenium/TestSimpleNotesApp.java
@@ -1,4 +1,5 @@
-package healenium; /**
+package healenium;
+/**
  * Healenium-appium Copyright (C) 2019 EPAM
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,27 +14,23 @@ package healenium; /**
 
 import com.epam.healenium.appium.DriverWrapper;
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TestSimpleNotesApp {
 
     private static AppiumDriver appiumDriver;
@@ -47,39 +44,52 @@ public class TestSimpleNotesApp {
         dc.setCapability(MobileCapabilityType.PLATFORM_NAME, "android");
         dc.setCapability("appPackage", "com.epam.healenium.appium.testapp");
         dc.setCapability("appActivity", ".MainActivity");
-        dc.setCapability(MobileCapabilityType.APP, "apps/simple-notes-app.apk");
+        dc.setCapability(MobileCapabilityType.APP, "D:\\dev\\github\\example-appium-mvn\\src\\test\\resources\\apps\\simple-notes-app.apk");
         dc.setCapability("test_data:testFindNotesButtonOk://*[contains(@text,'NOTES')]", "testClickNotesButtonHealed://*[contains(@text,'NNOTES')]");
 
         //declare delegate driver
         appiumDriver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), dc);
         //adding healing support
         appiumDriver = DriverWrapper.wrap(appiumDriver);
+        //appiumDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @SneakyThrows
     @Test
+    @Order(1)
     public void testFindNotesButtonOk() {
         MobileElement notesButtonOk = (MobileElement) appiumDriver.findElement(By.xpath("//*[contains(@text,'NOTES')]"));
     }
 
     @SneakyThrows
     @Test
+    @Order(2)
     public void testClickNotesButtonHealed() {
         MobileElement notesButtonHealed = (MobileElement) appiumDriver.findElement(By.xpath("//*[contains(@text,'NNOTES')]"));
         Assertions.assertEquals(notesButtonHealed.getText(), "NOTES");
         notesButtonHealed.click();
     }
 
+//    @SneakyThrows
+//    @Test
+//    @Order(3)
+//    public void testClickNoteOk() {
+//        MobileElement noteOk = (MobileElement) appiumDriver.findElement(By.xpath("//*[contains(@text,'Note 3')]"));
+//        noteOk.click();
+//    }
+
     @SneakyThrows
     @Test
-    public void testClickNotes3Ok() {
-        MobileElement note3Ok = (MobileElement) appiumDriver.findElement(By.xpath("//*[contains(@text,'Note 3')]"));
-        note3Ok.click();
+    @Order(3)
+    public void testClickPlusOk() {
+        MobileElement plusOk = (MobileElement) appiumDriver.findElementById("add_note_icon");
+        plusOk.click();
     }
 
     @AfterAll
     public static void tearDown() {
         if (appiumDriver != null) {
+            appiumDriver.removeApp("com.epam.healenium.appium.testapp.MainActivity");
             appiumDriver.quit();
         }
     }
